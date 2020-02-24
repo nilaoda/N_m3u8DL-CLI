126181051050a32dbd44dfab47ebef986b09ea83
nilaoda <nilaoda@live.com>
2020-02-24 17:05:01 +0800

    Update Downloader.cs

--- N_m3u8DL-CLI/Downloader.cs
+++ N_m3u8DL-CLI/Downloader.cs
@@ -131,7 +131,7 @@ namespace N_m3u8DL_CLI
                         //LOGGER.STOPLOG = true;  //停止记录日志
                     }
                     HLSLiveDownloader.REC_DUR += Convert.ToInt32(SegDur);
-                    if (HLSLiveDownloader.REC_DUR_LIMIT != -1 && HLSLiveDownloader.REC_DUR >= HLSLiveDownloader.REC_DUR_LIMIT) ;
+                    if (HLSLiveDownloader.REC_DUR_LIMIT != -1 && HLSLiveDownloader.REC_DUR >= HLSLiveDownloader.REC_DUR_LIMIT)
                     {
                         LOGGER.PrintLine("录制已到达限定长度", LOGGER.Warning);
                         LOGGER.WriteLine("录制已到达限定长度");
