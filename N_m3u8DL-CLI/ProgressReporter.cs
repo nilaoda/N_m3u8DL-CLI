612fc29197036b9925562b6dd70a12007c2191d0
nilaoda <nilaoda@live.com>
2021-09-02 22:41:16 +0800

    修复padding负值问题

--- N_m3u8DL-CLI/ProgressReporter.cs
+++ N_m3u8DL-CLI/ProgressReporter.cs
@@ -19,7 +19,9 @@ namespace N_m3u8DL_CLI
                 if (!string.IsNullOrEmpty(progress)) ProgressReporter.progress = progress;
                 if (!string.IsNullOrEmpty(speed)) ProgressReporter.speed = speed;
                 string now = DateTime.Now.ToString("HH:mm:ss.000");
-                string print = now + " " + ProgressReporter.progress + " " + ProgressReporter.speed + new string(' ', Console.WindowWidth - 4 - ProgressReporter.progress.Length - ProgressReporter.speed.Length - now.Length);
+                var sub = Console.WindowWidth - 4 - ProgressReporter.progress.Length - ProgressReporter.speed.Length - now.Length;
+                if (sub <= 0) sub = 0;
+                string print = now + " " + ProgressReporter.progress + " " + ProgressReporter.speed + new string(' ', sub);
                 Console.Write("\r" + print + "\r");
                 //Console.Write(print);
             }
